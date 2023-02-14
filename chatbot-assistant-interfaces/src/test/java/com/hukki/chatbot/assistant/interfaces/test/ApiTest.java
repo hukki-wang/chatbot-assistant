1a0f883a871b383d091fa8aa59326ec35c82204d
hukki-wang <18911946880@163.com>
2023-02-14 11:42:03 +0800

    爬取知识星球接口信息和chatpt接口信息：创建知识星球和chatgpt的测试类

--- chatbot-assistant-interfaces/src/test/java/com/hukki/chatbot/assistant/interfaces/test/ApiTest.java
+++ chatbot-assistant-interfaces/src/test/java/com/hukki/chatbot/assistant/interfaces/test/ApiTest.java
@@ -1,5 +1,18 @@
 package com.hukki.chatbot.assistant.interfaces.test;
 
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.junit.Test;
+
+import java.io.IOException;
+
 /**
  * @author YC02289 wanghui
  * @description: api测试类
@@ -7,4 +20,88 @@ package com.hukki.chatbot.assistant.interfaces.test;
  * @date 2023/2/14 10:37
  */
 public class ApiTest {
+
+    /**
+     * 1.创建知识星球
+     * 2.爬取等待我回答的接口信息
+     * 测试查询待我回答的问题
+     * @throws IOException
+     */
+    @Test
+    public void query_unanswered_questions() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet httpGet = new HttpGet("https://api.zsxq.com/v2/groups/48411118851818/topics?scope=unanswered_questions&count=20");
+        httpGet.addHeader("cookie", "知识星球个人cookie信息");
+        httpGet.addHeader("Content-Type", "application/json;charset=utf8");
+
+        CloseableHttpResponse response = httpClient.execute(httpGet);
+        int retCode = response.getStatusLine().getStatusCode();
+        if (retCode == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(retCode);
+        }
+    }
+
+    /**
+     * 爬取回答问题接口
+     */
+    @Test
+    public void answer() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/412884248251548/answer");
+        post.addHeader("cookie", "知识星球个人cookie信息");
+        post.addHeader("Content-Type", "application/json;charset=utf8");
+
+        String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"自己去百度！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": false\n" +
+                "  }\n" +
+                "}";
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        int retCode = response.getStatusLine().getStatusCode();
+        if (retCode == HttpStatus.SC_OK){
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        }else {
+            System.out.println(retCode);
+        }
+    }
+
+
+    /**
+     * 使用chatgpt回答问题
+     * @throws IOException
+     */
+    @Test
+    public void test_chatGPT() throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.openai.com/v1/completions");
+        post.addHeader("Content-Type", "application/json");
+        post.addHeader("Authorization", "Bearer 自行申请 https://beta.openai.com/overview");
+
+        String paramJson = "{\"model\": \"text-davinci-003\", \"prompt\": \"帮我写一个java冒泡排序\", \"temperature\": 0, \"max_tokens\": 1024}";
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));
+        post.setEntity(stringEntity);
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String res = EntityUtils.toString(response.getEntity());
+            System.out.println(res);
+        } else {
+            System.out.println(response.getStatusLine().getStatusCode());
+        }
+
+    }
 }
